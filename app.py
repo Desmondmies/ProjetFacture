0463d2b64c6817ac054cfdf811c04681d3879cc8
Thomas <thomproc@hotmail.fr>
2022-03-31 11:44:01 +0200

    Menu / template / var -> html
    Début bandeau du Menu.
    Création de template pour utiliser directement du code html.
    Passage et récupération de variable de python vers code html.

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-import os
-from flask import Flask
-
+from flask import Flask, render_template
+#render_template permet d'utiliser directement du code HTML
+#et de lui passer en paramètre des variables
 app = Flask(__name__)
 
+dico_client = {"Nom" : "noooom...",
+                "Prenom" : "...bril",
+                "Tel" : "06 06 06 06 06",
+                "Mail" : "machin@qqchmail.fr",
+                "Description" : "Riche client qui vient souvent etc..........."}
+
 @app.route("/")
-def test():
-    return "<h1>TEST HELLO</h1>"
+def home():
+    return render_template("home_page.html")
+
+@app.route("/autre")
+def autre():
+    return render_template("dynamique_page.html", posts = [dico_client]) #posts = variable à passer en paramètre à notre page HTML
+
 
 
 if __name__ == "__main__":
-	print("test github/atom")
+    print("test github/atom")
     try:
-        os.system('flask run')
+        app.run(debug = True)
     except KeyboardInterrupt:
         print("Application terminé.")
