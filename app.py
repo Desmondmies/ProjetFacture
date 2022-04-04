7e040ede19c27844ac7134086a43bf2b5f629df2
Thomas <thomproc@hotmail.fr>
2022-04-04 04:03:51 +0200

    structure html/css
    barre navigation ok
    structuration hiérarchie fichier : normalement plus qu'à les compléter

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import os
 from flask import Flask, render_template
 #render_template permet d'utiliser directement du code HTML
 #et de lui passer en paramètre des variables
-TEMPLATE_DIR = os.path.abspath('./WEB/HTML')
+
+HTML_DIR = os.path.abspath('./WEB/HTML')
 STATIC_DIR = os.path.abspath('./WEB/CSS')
 
-app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+app = Flask(__name__, template_folder=HTML_DIR, static_folder=STATIC_DIR)
 
 dico_client = {"Nom" : "noooom...",
                 "Prenom" : "...bril",
@@ -17,9 +18,22 @@ dico_client = {"Nom" : "noooom...",
 def home():
     return render_template("home_page.html")
 
-@app.route("/autre")
-def autre():
-    return render_template("dynamique_page.html", posts = [dico_client]) #posts = variable à passer en paramètre à notre page HTML
+@app.route("/facture")
+def facture():
+    return render_template("facture.html")
+
+@app.route("/devis")
+def devis():
+    return render_template("devis.html")
+
+@app.route("/client")
+def client():
+    return render_template("client.html", posts = [dico_client]) #posts = variable à passer en paramètre à notre page HTML
+
+@app.route("/artisan")
+def artisan():
+    return render_template("artisan.html")
+
 
 if __name__ == "__main__":
     print("test github/atom")
