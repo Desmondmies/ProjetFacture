b6b86c7982a7206ee38f399a86ee1ddca2d425a0
Thomas <thomproc@hotmail.fr>
2022-04-07 17:16:58 +0200

    Regroupement TP

--- Python/Facture.py
+++ Python/Facture.py
@@ -1,8 +1,8 @@
 class Facture:
-    def __init__(self, num, client, date_echeance, liste_produits = None):
+    def __init__(self, num: int, client, deadline, liste_produits:list = None) -> None:
         self.num = num
         self.client = client
         #self.date_creation = date_actuelle #CHECKER : date_echeance > date_creation
-        self.date_echeance = date_echeance
-        
+        self.deadline = deadline
+
         self.liste_produits = liste_produits
