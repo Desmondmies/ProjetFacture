144aba637f25d60e12ba8b8ba0e464cc00724d9c
rawi124 <99130108+rawi124@users.noreply.github.com>
2022-04-11 18:45:45 +0200

    third

--- fich.py
+++ fich.py
@@ -12,7 +12,7 @@ app = Flask(__name__,template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 @app.route("/")
 
 def home():
-    return render_template("./form.html")
+    return render_template("./facture.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
